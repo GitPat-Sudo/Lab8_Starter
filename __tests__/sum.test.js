5e0d8024da9b678e3ec77c821b12407c05297b35
GitPat-Sudo <p1wang@ucsd.edu>
2021-11-13 19:37:59 -0800

    sumpullshiit

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -5,9 +5,8 @@ test('adds 1 + 2 to equal 3', () => {
   expect(1 + 2).toBe(3);
 });
 
-const sum = require('../assets/scripts/sum.js');
-console.log(sum);
+const {sum} = require('../assets/scripts/sum.js');
 test('adds 1+2 to equal 3', ()=>{
-  expect(sum.sum(1,2)).toBe(3);
+  expect(sum(1,2)).toBe(3);
 });
 
